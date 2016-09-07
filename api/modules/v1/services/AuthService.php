446af25ed086ebb178cd2ef4d3d0716145e03da5
NingerJohn <ningerjohn@163.com>
2016-09-07 18:16:28 +0800

    add get distance function

--- api/modules/v1/services/AuthService.php
+++ api/modules/v1/services/AuthService.php
@@ -27,20 +27,24 @@ class AuthService extends \api\models\BaseModel
 	{
 		// 
 		$begin_time = $this->getMicrotime(2);
+		// 被替换的字符
 		$primary_arr = [
-			'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'
+			'a'=>'i','b'=>'l','c'=>'q','d'=>'6','e'=>'g','f'=>'2','g'=>'h','h'=>'7','i'=>'5','j'=>'v','k'=>'1','l'=>'4',
+			'm'=>'','n'=>'','o'=>'','p'=>'','q'=>'','r'=>'','s'=>'','t'=>'','u'=>'','v'=>'','w'=>'','x'=>'',
+			'y'=>'','z'=>'','1'=>'','2'=>'','3'=>'','4'=>'','5'=>'','6'=>'','7'=>'','8'=>'','9'=>'','0'=>'',
 		];
+		// 对应替换成的字符
 		$map_arr = [
-			'i','l','q','6','g','2','h','7','5','v','1','4','n','b','t','z','8','r','s','m','d','9','x','o','p','a','u','e','f','k','3','w','y','0','j','c'
+			'i','l','q','6','g','2','h','7','5','v','1','4','n','b','t','z','8','r','s','m','d','9','x','o','p','a','u','e','f','k','3','w','y','0','j','c',
 		];
-		// var_dump(md5('1'));exit; // c4ca4238a0b923820dcc509a6f75849b
-		$t = 'c4ca4238a0b923820dcc509a6f75849b';
-		$t_arr = str_split($t);
-		foreach ($t_arr as $k => $v) {
+		$t_arr = str_split($original_string); // 原字符串拆分成数组
+		foreach ($t_arr as $k => $v) { // 遍历处理
 			// 处理映射
 			$new[] = $map_arr[array_search($v, $primary_arr)];
 		}
 		// var_dump($new);
+		// var_dump(md5('1'));exit; // c4ca4238a0b923820dcc509a6f75849b
+		$t = 'c4ca4238a0b923820dcc509a6f75849b';
 		$end_time = $this->getMicrotime(2);
 		var_dump($begin_time);
 		var_dump($end_time);exit;
@@ -49,6 +53,38 @@ class AuthService extends \api\models\BaseModel
 		return $map_arr;
 	}
 
+	public function getDistance($begin_point=null, $end_point=null)
+	{
+		# code...
+		$distance = 6371.012 *
+		acos(cos(acos(-1) / 180 * $crt_latitude) *
+		cos(acos(-1) / 180 * $target_latitude) *
+		cos(acos(-1) / 180 * $crt_longitude - acos(-1) / 180 * $target_longitude) +
+		sin(acos(-1) / 180 * $crt_latitude) *
+		sin(acos(-1) / 180 * $target_latitude))*1;
+
+        $R = 6378137; // Earth’s mean radius in meter
+        $dLat = $this->rad($target_latitude - $crt_latitude);
+        $dLong = $this->rad($target_longitude - $crt_longitude);
+        $a = sin($dLat / 2) * sin($dLat / 2) +
+        cos($this->rad($crt_latitude)) * cos($this->rad($target_latitude)) *
+        sin($dLong / 2) * sin($dLong / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $d = $R * $c;
+        var_dump($d);exit;
+
+
+
+
+	}
+
+
+
+    public function rad($x=null)
+    {
+        # code...
+        return $x * pi() / 180;
+    }
 
 
 }
